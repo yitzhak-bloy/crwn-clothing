1572561d77e1c97925b78241e3ac644daba0e00f
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-01-23 14:34:37 +0200

    Bringing Shop Data To Our App

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -39,15 +39,39 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
   const collectionRef = firestore.collection(collectionKey);
 
   const batch = firestore.batch();
   objectsToAdd.forEach(obj => {
     const newDocRef = collectionRef.doc();
     batch.set(newDocRef, obj);
-  })
-  return await batch.commit()
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  console.log(transformedCollection)
+
+  // return transformedCollection.reduce((accumulator, collection) => {
+  //   accumulator[collection.title.toLowerCase()] = collection;
+  //   return accumulator;
+  // }, {});
 };
 
 export const auth = firebase.auth();
