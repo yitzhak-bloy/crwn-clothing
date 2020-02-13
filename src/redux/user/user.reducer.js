7894c19438936559944576737447d7ddf795bfb8
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-02-14 00:18:05 +0200

    created user sagas for sign up

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,4 +1,3 @@
-
 import UserActionTypes from './user.types';
 
 const INITIAL_STATE = {
@@ -22,6 +21,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
       };
     case UserActionTypes.SIGN_IN_FAILURE:
     case UserActionTypes.SIGN_OUT_FAILURE:
+    case UserActionTypes.SIGN_UP_FAILURE:
       return {
         ...state,
         error: action.payload
